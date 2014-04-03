76f966a2cf5c71398098b1f165adcdd916a84365
tilalahmad <tilal.ahmad@aspose.com>
2014-04-03 23:18:14 +0500

    updated HelloWorld

--- QuickStart/HelloWorld/gen/com/aspose/words/android/helloworld/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.aspose.words.android.helloworld;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
